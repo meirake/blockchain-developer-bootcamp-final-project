d34ada209eb389fff7ba407025381a895ee88630
Mareike Kuehn <ma.kuehn@gmx.ch>
2021-11-13 16:44:32 +0100

    Add web interface to createBaskets

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,14 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { web3: null, accounts: null, contract: null };
+  state = {
+    web3: null, 
+    accounts: null, 
+    contract: null,
+    hasBasket: false,
+    owner1: "",
+    owner2: ""
+  };
 
   componentDidMount = async () => {
     try {
@@ -22,7 +29,13 @@ class App extends Component {
         EscrowContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
-      this.setState({ web3, accounts, contract: instance });
+      this.setState({ 
+        web3: web3, 
+        accounts: accounts, 
+        contract: instance, 
+        owner1: accounts[0] 
+      });
+      await this.updateHasBasket();
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -32,13 +45,36 @@ class App extends Component {
     }
   };
 
+  async updateHasBasket() {
+    const hasBasket = await this.state.contract.methods.hasBasket().call(
+      {from: this.state.accounts[0]});
+    this.setState({hasBasket: hasBasket});
+  }
+
+  async makeBaskets() {
+    await this.state.contract.methods.createBaskets(
+      this.state.owner1, this.state.owner2).send({ from: this.state.owner1 }
+    );
+    await this.updateHasBasket();
+    console.log("Made baskets.")
+  }
+
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
+    if (this.state.hasBasket) {
+      return <div>Yeay, has a basket.</div>;
+    }
     return (
       <div className="App">
         <h1>Escrow</h1>
+        <input 
+          value={this.state.owner2}
+          type="text" 
+          onChange={(e)=>{this.setState({owner2: e.target.value})}}
+        /> 
+        <button onClick ={() => this.makeBaskets()}>Make baskets</button>
       </div>
     );
   }
