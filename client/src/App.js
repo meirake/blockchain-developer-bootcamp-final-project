e37beeec883066e7f195b55e135941d56efd74ca
Mareike Kuehn <ma.kuehn@gmx.ch>
2021-11-14 13:25:12 +0100

    Add viewing of baskets

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
     contract: null,
     hasBasket: false,
     account: "",
+    myBasket: [],
+    partnersBasket: []
   };
 
   componentDidMount = async () => {
@@ -35,6 +37,7 @@ class App extends Component {
         account: accounts[0] 
       });
       await this.updateHasBasket();
+      await this.updateBasketContents();
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -50,6 +53,28 @@ class App extends Component {
     this.setState({hasBasket: hasBasket});
   }
 
+  async updateBasketContents() {
+    await this.updateHasBasket();
+    if (!this.state.hasBasket) {
+      return;
+    }
+    let myTokens = [];
+    let pTokens = [];
+    const nrTokens = await this.state.contract.methods.viewNumberOfDepositedTokens().call({from: this.state.account});
+    for(let i = 0; i < nrTokens[0]; ++i) {
+      const token = await this.state.contract.methods.viewMyBasket(i).call({from: this.state.account});
+      myTokens.push(token);
+      // console.log(token[1]);
+    }
+    for(let i = 0; i < nrTokens[1]; ++i) {
+      const token = await this.state.contract.methods.viewPartnerBasket(i).call({from: this.state.account});
+      pTokens.push(token);
+    }
+    this.setState({myBasket: myTokens, partnersBasket: pTokens});
+    console.log(this.state.myBasket);
+    console.log(this.state.partnersBasket);
+  }
+
   async makeBaskets(partner) {
     await this.state.contract.methods.createBaskets(
       this.state.account, partner).send({ from: this.state.account }
@@ -63,7 +88,16 @@ class App extends Component {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     if (this.state.hasBasket) {
-      return <div>Yeay, has a basket.</div>;
+      return (
+        <div className="App">
+          <h1>Your open transaction</h1>
+          <ViewBaskets 
+            myBasket = {this.state.myBasket}
+            pBasket = {this.state.partnersBasket}
+            onClick={() => this.updateBasketContents()} 
+          />
+        </div>
+      );
     }
     return (
       <div className="App">
@@ -71,6 +105,49 @@ class App extends Component {
         <CreateTransaction
           onClick={(partner) => this.makeBaskets(partner)}
         />
+      </div>
+    );
+  }
+}
+
+class ViewBaskets extends Component {
+  basketElements(basket) {
+    const tokens = basket.map((token, index) => {
+      return (
+        <li key={index}>
+          <p>Token address:<br/>
+          {token[0]}<br/>
+          Token ID:<br/>
+          {token[1]}
+          </p>
+        </li>
+      )
+    });
+    return tokens;
+  }
+
+  render(){
+    return(
+      <div>
+        <div>
+          <button onClick={() => this.props.onClick()}>
+            UpdateBaskets
+          </button>
+        </div>
+        <div className="row">
+          <div className = "col">
+            <h2>My Basket</h2>
+            <ul className ="basket">
+              {this.basketElements(this.props.myBasket)}
+            </ul>
+          </div>
+          <div className = "col">
+            <h2>Partner Basket</h2>
+            <ul className ="basket">
+              {this.basketElements(this.props.pBasket)}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
